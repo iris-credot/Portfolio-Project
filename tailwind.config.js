eb44c1d8fca4229a13311788aed365934af4968d
iris-credot <tetairiscredot@gmail.com>
2025-04-12 19:33:27 +0200

    updated the portfolio and added readme file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', 
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
